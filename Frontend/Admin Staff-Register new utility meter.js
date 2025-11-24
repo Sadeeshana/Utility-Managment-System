853f7325b8768b6715580b2134f0530930d2043f
Isurindi <nwlidayarathna@students.nsbm.ac.lk>
2025-11-24 23:37:48 +0530

    Delete Admin Staff-Register new utility meter  files
    Delete Admin Staff-Register new utility meter files

--- Frontend/Admin Staff-Register new utility meter.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("meterForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Meter Registered Successfully!");
-});
-
-document.querySelector(".cancel-btn").addEventListener("click", function () {
-    document.getElementById("meterForm").reset();
-});

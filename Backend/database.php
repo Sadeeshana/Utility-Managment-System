3c8fa2ca8dc91198a2eeab1072a166c6ecfb7068
Sadeeshana <sadeeshanaakash16@gmail.com>
2025-11-24 18:14:56 +0530

    Move files to the frontend folder

--- Backend/database.php
+++ Backend/database.php
@@ -1,26 +1,19 @@
 <?php
-// --- Database Connection Variables ---
-$serverName = "SADEESHANA2004\SQLEXPRESS"; // Your server name
+$serverName = "SADEESHANA2004\SQLEXPRESS"; 
 $connectionOptions = [
-    "Database" => "Utility_Management_System", // Your database name
-    "Uid" => "Sadeeshana",        // Your SQL Server login username
-    "PWD" => "sadeeputha"         // Your SQL Server login password
+    "Database" => "Utility_Management_System", 
+    "Uid" => "Sadeeshana",        
+    "PWD" => "sadeeputha"         
 ];
 
-// --- Establishes the connection ---
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 
-// --- Check the connection ---
 if ($conn === false) {
     // Connection failed, print the errors
     echo "Connection failed.<br />";
     die(print_r(sqlsrv_errors(), true));
 } else {
-    // Connection was successful
-    // echo "Connected successfully.<br />";
+   
 }
-// You can now run queries here...
 
-// Close the connection when done
-// sqlsrv_close($conn);
 ?>
